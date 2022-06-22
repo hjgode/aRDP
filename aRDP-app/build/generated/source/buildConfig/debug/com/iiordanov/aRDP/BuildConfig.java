43dc6a87f9fd2e5e722da46af70e460eba6c2710
hjgode@gmail.com <hjgode@gmail.com>
2022-06-22 17:22:19 +0200

    first commit

---
+++ aRDP-app/build/generated/source/buildConfig/debug/com/iiordanov/aRDP/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.iiordanov.aRDP;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.iiordanov.aRDP";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 115104;
+  public static final String VERSION_NAME = "v5.1.0";
+}
